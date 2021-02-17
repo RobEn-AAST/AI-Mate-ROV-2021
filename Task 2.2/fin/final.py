72c60f36b9ff06e6f601869356800fbcf3a7af13
mhwahdan <moustafawahdan55@gmail.com>
2021-02-17 16:43:17 +0200

    changed the code to work with video stream

--- Task 2.2/fin/final.py
+++ Task 2.2/fin/final.py
@@ -5,87 +5,6 @@
 import numpy as np
 import imutils
 import cv2
-import argparse
-
-
-def align_images(image, template, maxFeatures=500, keepPercent=0.2,
-                 debug=False):
-    # convert both the input image and template to grayscale
-    imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-
-    # use ORB to detect keypoints and extract (binary) local
-    # invariant features
-    orb = cv2.ORB_create(maxFeatures)
-    (kpsA, descsA) = orb.detectAndCompute(imageGray, None)
-    (kpsB, descsB) = orb.detectAndCompute(templateGray, None)
-
-    # match the features
-    method = cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
-    matcher = cv2.DescriptorMatcher_create(method)
-    matches = matcher.match(descsA, descsB, None)
-
-    # sort the matches by their distance (the smaller the distance,
-    # the "more similar" the features are)
-    matches = sorted(matches, key=lambda x: x.distance)
-
-    # keep only the top matches
-    keep = int(len(matches) * keepPercent)
-    matches = matches[:keep]
-
-    # check to see if we should visualize the matched keypoints
-    if debug:
-        matchedVis = cv2.drawMatches(image, kpsA, template, kpsB,
-                                     matches, None)
-        matchedVis = imutils.resize(matchedVis, width=1000)
-        cv2.waitKey(0)
-
-    # allocate memory for the keypoints (x,y-coordinates) from the
-    # top matches -- we'll use these coordinates to compute our
-    # homography matrix
-    ptsA = np.zeros((len(matches), 2), dtype="float")
-    ptsB = np.zeros((len(matches), 2), dtype="float")
-
-    # loop over the top matches
-    for (i, m) in enumerate(matches):
-        # indicate that the two keypoints in the respective images
-        # map to each other
-        ptsA[i] = kpsA[m.queryIdx].pt
-        ptsB[i] = kpsB[m.trainIdx].pt
-
-    # compute the homography matrix between the two sets of matched
-    # points
-    (H, mask) = cv2.findHomography(ptsA, ptsB, method=cv2.RANSAC)
-
-    # use the homography matrix to align the images
-    (h, w) = template.shape[:2]
-    aligned = cv2.warpPerspective(image, H, (w, h))
-
-    # return the aligned image
-    return aligned
-
-
-# construct the argument parser and parse the arguments
-parser = argparse.ArgumentParser(description='task 2.2\nthis code takes 2 images and processes then and prints the output on the screen and stores it in a file called result.jpg')
-parser.add_argument('-O','--old', metavar='', type=str,
-                    help='the path of the old image',required=True)
-parser.add_argument('-N','--new', metavar='', type=str,
-                    help='the path of the new image',required=True)
-
-args = parser.parse_args()
-# load the input image and template from disk
-print("[INFO] loading images...")
-image = cv2.imread(args.new)
-template = cv2.imread(args.old)
-
-# align the images
-print("[INFO] aligning images...")
-aligned = align_images(image, template, debug=True)
-
-# resize both the aligned and template images so we can easily
-# visualize them on our screen
-aligned = imutils.resize(aligned, width=700)
-template = imutils.resize(template, width=700)
 
 
 # define images class
@@ -101,7 +20,7 @@ class changes_detector:
         self.upper_purple = np.array([170, 255, 255])
         self.lower_white = np.array([0, 3, 220])
         self.upper_white = np.array([255, 255, 255])
-        self.kernel = np.ones((2, 2), np.uint8)
+        self.kernel = np.ones((5, 5), np.uint8)
         self.kernel2 = np.ones((15, 15), np.uint8)
     def detect(self, old_image, new_image):
         preview = new_image.original.copy()
@@ -154,55 +73,136 @@ class changes_detector:
         blotching_cnts = cv2.findContours(blotching_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
         recovery_cnts = cv2.findContours(recovery_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
         # draw the contours on the preview image
+        return { 'growth_cnts' : growth_cnts,'death_cnts' : death_cnts,'blotching_cnts' : blotching_cnts,'recovery_cnts' : recovery_cnts }
+
+def align_images(image1, template, maxFeatures=500, keepPercent=0.2,
+                 debug=False,detector = changes_detector):
+    hsv = cv2.cvtColor(image1, cv2.COLOR_BGR2HSV)
+    lower_val = np.array([130, 50, 90])
+    upper_val = np.array([170, 255, 255])
+    mask = cv2.inRange(hsv, lower_val, upper_val)
+    if np.sum(mask) <= 1000:
+        return { 'growth_cnts' : [],'death_cnts' : [],'blotching_cnts' : [],'recovery_cnts' : [] }
+    # convert both the input image and template to grayscale
+    imageGray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+    # use ORB to detect keypoints and extract (binary) local
+    # invariant features
+    orb = cv2.ORB_create(maxFeatures)
+    (kpsA, descsA) = orb.detectAndCompute(imageGray, None)
+    (kpsB, descsB) = orb.detectAndCompute(templateGray, None)
+
+    # match the features
+    method = cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
+    matcher = cv2.DescriptorMatcher_create(method)
+    matches = matcher.match(descsA, descsB, None)
+
+    # sort the matches by their distance (the smaller the distance,
+    # the "more similar" the features are)
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    # keep only the top matches
+    keep = int(len(matches) * keepPercent)
+    matches = matches[:keep]
+    if len(matches) < 50:
+        return { 'growth_cnts' : [],'death_cnts' : [],'blotching_cnts' : [],'recovery_cnts' : [] }
+    # check to see if we should visualize the matched keypoints
+    if debug:
+        matchedVis = cv2.drawMatches(image1, kpsA, template, kpsB,
+                                     matches, None)
+        matchedVis = imutils.resize(matchedVis, width=1000)
+
+    # allocate memory for the keypoints (x,y-coordinates) from the
+    # top matches -- we'll use these coordinates to compute our
+    # homography matrix
+    ptsA = np.zeros((len(matches), 2), dtype="float")
+    ptsB = np.zeros((len(matches), 2), dtype="float")
+
+    # loop over the top matches
+    for (i, m) in enumerate(matches):
+        # indicate that the two keypoints in the respective images
+        # map to each other
+        ptsA[i] = kpsA[m.queryIdx].pt
+        ptsB[i] = kpsB[m.trainIdx].pt
+
+    # compute the homography matrix between the two sets of matched
+    # points
+    if len(ptsA) == 0 or len(ptsB) == 0:
+        return { 'growth_cnts' : [],'death_cnts' : [],'blotching_cnts' : [],'recovery_cnts' : [] }
+    (H, mask) = cv2.findHomography(ptsA, ptsB, method=cv2.RANSAC)
+    # use the homography matrix to align the images
+    (h, w) = template.shape[:2]
+    aligned = image(cv2.warpPerspective(image1, H, (w, h)))
+    old = image(template)
+    # return the aligned image
+    return detector.detect(old,aligned)
+
+# load the input image and template from disk
+template = cv2.imread('old.png')
+
+# align the images
+
+# resize both the aligned and template images so we can easily
+# visualize them on our screen
+
+# load the image
+old_image = image(template)
+
+# create images detector
+imagedetector = changes_detector()
+
+cap = cv2.VideoCapture(0)
+while not cap.isOpened():
+    cap = cv2.VideoCapture(0)
+    cv2.waitKey(1000)
+    print ("Wait for the header")
+
+while True:
+    flag, frame = cap.read() # get the frame
+    if flag:
+
+        contours = align_images(frame.copy(), template, debug=True,detector= imagedetector)
         area = 120
-        for c in growth_cnts:
+        for c in contours.get('growth_cnts'):
             if cv2.contourArea(c) > area:
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
                 x, y, w, h = cv2.boundingRect(approx)
                 # green
-                cv2.rectangle(preview, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(preview, "growth " , (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(frame, "growth " , (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
                             (0, 255, 0), 2)
-        for c in death_cnts:
+        for c in contours.get('death_cnts'):
             if cv2.contourArea(c) > area:
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
                 x, y, w, h = cv2.boundingRect(approx)
                 # cyan
-                cv2.rectangle(preview, (x, y), (x + w, y + h), (0, 255, 255), 2)
-                cv2.putText(preview, "death ", (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+                cv2.putText(frame, "death ", (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
                             (0, 255, 255), 2)
-        for c in blotching_cnts:
+        for c in contours.get('blotching_cnts'):
             if cv2.contourArea(c) > area:
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
                 x, y, w, h = cv2.boundingRect(approx)
                 # blue
-                cv2.rectangle(preview, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                cv2.putText(preview, "blotching ", (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.putText(frame, "blotching ", (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
                             (0, 0, 255), 2)
-        for c in recovery_cnts:
+        for c in contours.get('recovery_cnts'):
             if cv2.contourArea(c) > area:
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
                 x, y, w, h = cv2.boundingRect(approx)
-                cv2.rectangle(preview, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 # red
-                cv2.putText(preview, "recovery " , (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                cv2.putText(frame, "recovery " , (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,
                             (255,0 , 0), 2)
+    cv2.imshow("result",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        return np.hstack([old_image.original, new_image.original, preview])
-
-
-# load the image
-old_image = image(template)
-cv2.waitKey(0)
-new_image = image(aligned)
-# create images detector
-imagedetector = changes_detector()
-# draw output
-result = imagedetector.detect(old_image, new_image)
-cv2.imshow("result", result)
-cv2.imwrite("result.jpg",result)
-cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows()
