4a58feaa038647525f80475147c52fda044acb04
mhwahdan <moustafawahdan55@gmail.com>
2021-02-25 20:21:50 +0200

    added debug mode

--- Task 2.2/object_detection_module.py
+++ Task 2.2/object_detection_module.py
@@ -6,9 +6,8 @@ upper_purple = np.array([170, 255, 255])
 lower_white = np.array([0, 3, 220])
 upper_white = np.array([255, 255, 255])
 kernel = np.ones((5, 5), np.uint8)
-kernel2 = np.ones((15, 15), np.uint8)
 
-def extract(image):
+def extract(image,debug = False):
     purple_mask = cv2.inRange(image, lower_purple,upper_purple)
     white_mask = cv2.inRange(image, lower_white, upper_white)
     white  = cv2.bitwise_and(image, image, mask=white_mask)
@@ -16,17 +15,21 @@ def extract(image):
     white[white_mask > 0] = (255, 255, 255)
     purple[purple_mask > 0] = (255, 255, 255)
     white = cv2.morphologyEx(white, cv2.MORPH_OPEN,kernel)
+    if debug:
+        cv2.imshow("extraction result",np.hstack([image,purple,white]))
     return { 'original' : image,'white' : white,'purple' : purple}
 
-def togrey(image_dict):
+def togrey(image_dict,debug = False):
     image = cv2.cvtColor(image_dict.get('original'), cv2.COLOR_BGR2GRAY)
     purple = cv2.cvtColor(image_dict.get('white'), cv2.COLOR_BGR2GRAY)
     white = cv2.cvtColor(image_dict.get('purple'), cv2.COLOR_BGR2GRAY)
     purple = cv2.threshold(purple, 127, 255, cv2.THRESH_BINARY)[1]
     white = cv2.threshold(white, 127, 255, cv2.THRESH_BINARY)[1]
+    if debug:
+        cv2.imshow("extraction result",np.hstack([image,purple,white]))
     return { 'original' : image,'white' : white,'purple' : purple}
 
-def detect(new_image, old_image,fgmask):
+def detect(new_image, old_image,fgmask,debug = False):
     oldimage = extract(old_image)
     newimage = extract(new_image)
     oldimage_g = togrey(oldimage)
@@ -35,7 +38,6 @@ def detect(new_image, old_image,fgmask):
     common_purple = cv2.bitwise_xor(fgmask, newimage_g.get('purple'))
     common_purple = cv2.subtract(common_purple,oldimage_g.get('white'))
     common_purple = cv2.subtract(common_purple,newimage_g.get('white'))
-    cv2.imshow("common purple",common_purple)
     # growth = new image && common part
     growth = cv2.bitwise_and(newimage_g.get('purple'), common_purple)
     # death  = old image && common
@@ -46,20 +48,24 @@ def detect(new_image, old_image,fgmask):
     recovery = cv2.bitwise_and(newimage.get('purple'), oldimage.get('white'))
     # apply opening morphology on results  to reduce noise
     growth = cv2.morphologyEx(growth, cv2.MORPH_OPEN, kernel)
-    death = cv2.morphologyEx(death, cv2.MORPH_OPEN, kernel2)
+    death = cv2.morphologyEx(death, cv2.MORPH_OPEN, kernel)
     blotching = cv2.morphologyEx(blotching, cv2.MORPH_OPEN, kernel)
     recovery = cv2.morphologyEx(recovery, cv2.MORPH_OPEN, kernel)
     # transform the growth and death images to the grey scale domain to get contours
-    blotching_grey_scale = cv2.cvtColor(blotching, cv2.COLOR_BGR2GRAY)
-    recovery_grey_scale = cv2.cvtColor(recovery, cv2.COLOR_BGR2GRAY)
-    growth_thresh = cv2.threshold(growth, 127, 255, 0)[1]
-    death_thresh = cv2.threshold(death, 127, 255, 0)[1]
-    blotching_thresh = cv2.threshold(blotching_grey_scale, 127, 255, 0)[1]
-    recovery_thresh = cv2.threshold(recovery_grey_scale, 127, 255, 0)[1]
+    blotching = cv2.cvtColor(blotching, cv2.COLOR_BGR2GRAY)
+    recovery = cv2.cvtColor(recovery, cv2.COLOR_BGR2GRAY)
+    blotching = cv2.threshold(blotching, 127, 255, 0)[1]
+    recovery = cv2.threshold(recovery, 127, 255, 0)[1]
     # find the contours
-    growth_cnts = cv2.findContours(growth_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
-    death_cnts = cv2.findContours(death_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
-    blotching_cnts = cv2.findContours(blotching_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
-    recovery_cnts = cv2.findContours(recovery_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+    growth_cnts = cv2.findContours(growth, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+    death_cnts = cv2.findContours(death, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+    blotching_cnts = cv2.findContours(blotching, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+    recovery_cnts = cv2.findContours(recovery, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
     # draw the contours on the preview image
+    if debug:
+        cv2.imshow("common purple",common_purple)
+        cv2.imshow("growth",growth)
+        cv2.imshow("death",death)
+        cv2.imshow("blotching",blotching)
+        cv2.imshow("recovery",recovery)
     return { 'growth_cnts' : growth_cnts,'death_cnts' : death_cnts,'blotching_cnts' : blotching_cnts,'recovery_cnts' : recovery_cnts }
--- Task 2.2/task_2-2.py
+++ Task 2.2/task_2-2.py
@@ -9,33 +9,32 @@ flag = False
 points = []
 
 # Get default camera window size
-ret, frame = cap.read()
+frame = cap.read()[1]
 Height, Width = frame.shape[:2]
 old_image = cv2.imread("old.png")
 old_image = cv2.resize(old_image,(Width,Height))
 frame_count = 0
-kernel3 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-print("learning background ... please dont put any thing in the ROV way !!!")
-backSub = cv2.createBackgroundSubtractorMOG2()
-print("Background subtractor created successfully... just a few more seconds")
+#create back ground subtraction model using MOG2 algorithm
+backSub = cv2.createBackgroundSubtractorMOG2() 
+## To change to KNN Algorithm replace the previous line with >> backSub = cv2.createBackgroundSubtractorKNN()
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+
 while True:
 
     # Capture webcame frame
-    ret, frame = cap.read()
+    frame = cap.read()[1]
     hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #update the background model
     fgMask = backSub.apply(frame,15000)
-    fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_OPEN, kernel3)
+    fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_OPEN, kernel)
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv_img, object_detection_module.lower_purple, object_detection_module.upper_purple)
     
-    # Find contours, OpenCV 3.X users use this line instead
-    _, contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
+    _, contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    
     # Create empty centre array to store centroid center of mass
     center =   int(Height/2), int(Width/2)
-
     if len(contours) > 0:
         
         # Get the largest contour and its center 
@@ -44,16 +43,13 @@ while True:
         M = cv2.moments(c)
         try:
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
         except:
             center =   int(Height/2), int(Width/2)
-
         # Allow only countors that have a larger than 15 pixel radius
         if radius > 25:
-            
             # Draw cirlce and leave the last center creating a trail
             #use our function here
-            contours = object_detection_module.detect(cv2.flip(frame, 1),old_image,fgMask)
+            contours = object_detection_module.detect(cv2.flip(frame, 1),old_image,fgMask,debug = True)
             # Log center points 
             points.append(center)
             frame_count = 0
@@ -107,7 +103,6 @@ while True:
                 # red
                 cv2.putText(frame, "recovery " , (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,(255,0 , 0), 2)
     cv2.imshow("Object Tracker", frame)
-    cv2.imshow('FG Mask', fgMask)
     if cv2.waitKey(1) == 13: #13 is the Enter Key
         break
 
