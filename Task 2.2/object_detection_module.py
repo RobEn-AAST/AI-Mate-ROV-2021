1d19232dbe12b312d16a9bbe817eb7b5ed7f2f02
moustafa wahdan <moustafawahdan55@gmail.com>
2021-03-07 21:04:16 +0200

    updated the code to fit coding stadards + added a function that instructs the rov to move such that the best distance between it and object is acheived

--- Task 2.2/object_detection_module.py
+++ Task 2.2/object_detection_module.py
@@ -1,12 +1,13 @@
+""" Module that contains functions , objects and constants used in task 2.2"""
 import numpy as np
-import cv2
+from cv2 import cv2
 
 # Defining module Constants
 LOWER_PURPLE = np.array([130, 50, 90]) # Purple HSV lower boundry
 UPPER_PURPLE = np.array([170, 255, 255]) # Purple HSV upper boundry
-LOWER_WHITE = np.array([0, 3, 220]) # White HSV lower boundry
+LOWER_WHITE = np.array([0, 6, 180]) # White HSV lower boundry
 UPPER_WHITE = np.array([255, 255, 255]) # White HSV upper boundry
-KERNEL_ALLIGN = np.ones((2, 2), np.uint8) # Kernel used for opening effect
+KERNEL_ALLIGN = np.ones((5, 5), np.uint8) # Kernel used for opening effect
 MAX_FEATURES = 100 # Maximum number of features to be detected
 GOOD_MATCH_PERCENT = 0.15 # Matching tolerence
 AREA = 250 # Minimum contour area
@@ -14,17 +15,18 @@ GREEN = (0, 255, 0) # Green BGR code
 YELLOW = (0, 255, 255) # Yellow BGR code
 RED = (0, 0, 255) # Red BGR code
 BLUE = (255, 0, 0) # Blue BGR code
+AREA_TOLERANCE = 1000 # Difference in area tolerence
 
 # Objects used for feature matching
 bf = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
 orb = cv2.ORB_create(MAX_FEATURES)
 
-# Python function that checks if 2 images match and returns if they match and the number of good matches
 def check_for_matches(old,new,debug = False):
+    """ checks if 2 images match and returns if they match and the number of good matches """
+    #detect matches and sort them
     matches = bf.match(cv2.cvtColor(old, cv2.COLOR_BGR2GRAY),cv2.cvtColor(new, cv2.COLOR_BGR2GRAY))
     matches.sort(key=lambda x: x.distance, reverse=False)
   # Remove not so good matches
-    matches.sort(key=lambda x: x.distance, reverse=False)
     numgoodmatches = int(len(matches) * GOOD_MATCH_PERCENT)
     matches = matches[:numgoodmatches]
     num_of_matches = len(matches)
@@ -33,26 +35,9 @@ def check_for_matches(old,new,debug = False):
         print("matching percentage = " + str( (numgoodmatches/MAX_FEATURES) * 100))
     return (num_of_matches >= MAX_FEATURES,numgoodmatches)
 
-#python function that uses feature matching + homography to allign the new image
-def align(old,new,debug = False):
-    keypoints1, descriptors1 = orb.detectAndCompute(cv2.cvtColor(old, cv2.COLOR_BGR2GRAY), None)
-    keypoints2, descriptors2 = orb.detectAndCompute(cv2.cvtColor(new, cv2.COLOR_BGR2GRAY), None)
-    matches = bf.match(descriptors1, descriptors2, None)
-    points1 = np.zeros((len(matches), 2), dtype=np.float32)
-    points2 = np.zeros((len(matches), 2), dtype=np.float32)
-    for i, match in enumerate(matches):
-        points1[i, :] = keypoints1[match.queryIdx].pt
-        points2[i, :] = keypoints2[match.trainIdx].pt
-    h = cv2.findHomography(points1, points2, cv2.RANSAC)[0]
-    height, width = (old.shape[0],old.shape[1])
-    output = cv2.warpPerspective(new, h, (width, height))
-   # Print debug info if debug mode is on    
-    if debug:
-        cv2.imshow("alligned image",output)
-    return output
 
-# Python function that extracts purple and white parts out of an image
 def extract(image,debug = False):
+    """ Python function that extracts purple and white parts out of an image """
     purple_mask = cv2.inRange(image, LOWER_PURPLE,UPPER_PURPLE)
     white_mask = cv2.inRange(image, LOWER_WHITE, UPPER_WHITE)
     white  = cv2.bitwise_and(image, image, mask=white_mask)
@@ -60,25 +45,26 @@ def extract(image,debug = False):
     white[white_mask > 0] = (255, 255, 255)
     purple[purple_mask > 0] = (255, 255, 255)
     white = cv2.morphologyEx(white, cv2.MORPH_OPEN,KERNEL_ALLIGN)
-   # Print debug info if debug mode is on    
+   # Print debug info if debug mode is on
     if debug:
         cv2.imshow("extraction result",np.hstack([image,purple,white]))
     return { 'original' : image,'white' : white,'purple' : purple}
 
-# Python function that changes image to black and white
 def to_black_and_white(image_dict,debug = False):
+    """ Python function that changes image to black and white """
     image = cv2.cvtColor(image_dict.get('original'), cv2.COLOR_BGR2GRAY)
     purple = cv2.cvtColor(image_dict.get('white'), cv2.COLOR_BGR2GRAY)
     white = cv2.cvtColor(image_dict.get('purple'), cv2.COLOR_BGR2GRAY)
     purple = cv2.threshold(purple, 127, 255, cv2.THRESH_BINARY)[1]
     white = cv2.threshold(white, 127, 255, cv2.THRESH_BINARY)[1]
-   # Print debug info if debug mode is on    
+   # Print debug info if debug mode is on
     if debug:
         cv2.imshow("extraction result",np.hstack([image,purple,white]))
     return { 'original' : image,'white' : white,'purple' : purple}
 
-# Python function that detects the changes in the new image returning contours at their places
 def detect(new_image, old_image,debug = False):
+    """ detects the changes in the new image returning
+         contours at their places """
     oldimage = extract(old_image)
     newimage = extract(new_image)
     # find the common purple part between the 2 images using bitwise xor
@@ -113,12 +99,38 @@ def detect(new_image, old_image,debug = False):
     return { 'growth_cnts' : growth_cnts,
     'death_cnts' : death_cnts,'blotching_cnts' : blotching_cnts,'recovery_cnts' : recovery_cnts }
 
-# Python function that filters the contours according to a tolerence area
 def filter_contours(image,contours,color,text,area = 150):
-    for c in contours:
-        if cv2.contourArea(c) > area:
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-            x, y, w, h = cv2.boundingRect(approx)
-            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(image, text , (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7,color, 2)
+    """ Python function that filters the contours according to a tolerence area"""
+    for _c in contours:
+        if cv2.contourArea(_c) > area:
+            peri = cv2.arcLength(_c, True)
+            approx = cv2.approxPolyDP(_c, 0.02 * peri, True)
+            _x, _y, _w, _h = cv2.boundingRect(approx)
+            cv2.rectangle(image, (_x, _y), (_x + _w, _y + _h), color, 2)
+            cv2.putText(image, text , (_x + _w + 20, _y + 20), cv2.FONT_HERSHEY_COMPLEX,0.7,color,2)
+
+def remove_back_ground(image):
+    """ utilizes the color extraction function extract() to remove background from image """
+    parts = extract(image,debug=False)
+    return cv2.bitwise_xor(parts.get('purple'),parts.get('white'))
+
+def get_colony_area(image):
+    """ draws rectangle around the white chunk of color and calculate its area """
+    image  = cv2.cvtColor(remove_back_ground(image),cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("result1.png",image)
+    contours = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    _c = max(contours, key = cv2.contourArea)
+    return cv2.contourArea(_c)
+
+def adjust_distance(oldimage,frame):
+    """ compares 2 objects in 2 images and commands the ROV to move in order to adjust them """
+    frame_d = get_colony_area(frame)
+    oldimage_d = get_colony_area(oldimage)
+    if oldimage_d - AREA_TOLERANCE < frame_d and oldimage_d + AREA_TOLERANCE > frame_d :
+        return "Distance is perfect !!!"
+    elif oldimage_d > frame_d :
+        return "move forward !!!"
+    else :
+        return "move backward !!!"
+
+# End of module
---
+++ Task 2.2/task_2_2.py
@@ -0,0 +1,52 @@
+""" Driver code for task 2.2"""
+from cv2 import cv2 as cv
+import object_detection_module as od
+
+# Initalize camera
+cap = cv.VideoCapture(0)
+
+# Get default camera window size
+frame = cap.read()[1]
+Height, Width = frame.shape[:2]
+
+# Read old image and resize it to the size of the camera dimensions
+old_image = cv.imread("old.png")
+old_image = cv.resize(old_image,(Width,Height))
+
+# Define Script constants
+SAMPLING_RATE = 250
+DEBUG = False # Select debug mode
+
+#Define Script variables
+FRAME_COUNT = 0 # Frames tracking variable
+SAMPLE_COUNTER = 0 # Samples trackingcounter
+contours = { 'growth_cnts' : [],'death_cnts' : [],'blotching_cnts' : [] ,'recovery_cnts' : [] }
+while True:
+    # Capture webcame frame
+    frame = cv.flip(cap.read()[1],1)
+    is_matching,number_of_matches = od.check_for_matches(frame,old_image,debug = DEBUG)
+    # If the current frame matches the old image
+    if is_matching:
+        #Increment the sample counter by 1
+        SAMPLE_COUNTER += 1
+        if SAMPLE_COUNTER > SAMPLING_RATE:
+            contours = od.detect(frame,old_image,debug = DEBUG) # Detect changes
+            SAMPLE_COUNTER = 0 # Reset the sampling counter
+            #filter contours to get only large ones which eliminates contours caused by noise
+            od.filter_contours(frame,contours.get('growth_cnts'),od.GREEN,"growth",area = od.AREA)
+            od.filter_contours(frame,contours.get('death_cnts'),od.YELLOW,"death",area = od.AREA)
+            od.filter_contours(frame,contours.get('blotching_cnts'),od.RED,"blotching",area=od.AREA)
+            od.filter_contours(frame,contours.get('recovery_cnts'),od.BLUE,"recovery",area=od.AREA)
+    else:
+        SAMPLE_COUNTER = 0 # Reset sample counter
+        contours = {'growth_cnts':[],'death_cnts':[],'blotching_cnts' :[],'recovery_cnts':[]}
+    # Display the result
+    cv.imshow("Object Tracker", frame)
+    FRAME_COUNT += 1
+    #close the stream when enter key is pressed
+    if cv.waitKey(1) == 13: #13 is the Enter Key
+        break
+
+# Release camera and close any open windows
+cap.release()
+cv.destroyAllWindows()
