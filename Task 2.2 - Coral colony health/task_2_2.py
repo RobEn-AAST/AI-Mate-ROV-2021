c037a10a50667580b388f0a316f206831a86ebcd
princeofguilty <princeofguilty@hotmail.com>
2021-04-29 00:17:10 +0200

    some merge fix

--- Task 2.2 - Coral colony health/task_2_2.py
+++ Task 2.2 - Coral colony health/task_2_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """ Driver code for task 2.2"""
 import cv2 as cv
 from object_detection_module import COLORS, AREA, simplest_cb, adjust_angle, extract
@@ -68,66 +67,3 @@ while True:
 # Release camera and close any open windows
 cap.release()
 cv.destroyAllWindows()
-=======
-""" Driver code for task 2.2"""
-import cv2 as cv
-from object_detection_module import COLORS, AREA, simplest_cb, adjust_angle, extract
-from object_detection_module import check_for_matches, detect, print_contours, adjust_distance
-
-# Initalize camera
-cap = cv.VideoCapture(0)
-
-# Get default camera window size
-frame = cap.read()[1]
-Height, Width = frame.shape[:2]
-
-# Read old image and resize it to the size of the camera dimensions
-old_image = cv.imread("old.png")
-old_image = simplest_cb(cv.resize(old_image, (Width, Height)), 1)
-old_image_ext = extract(adjust_angle(frame=old_image))
-# Define Script constants
-SAMPLING_RATE = 250
-DEBUG = False  # Select debug mode
-MANUAL = False  # Turn manual mode off and on
-# Define Script variables
-FRAME_COUNT = 0  # Frames tracking variable
-SAMPLE_COUNTER = 0  # Samples trackingcounter
-SIZE_TOLERENCE = 0.3  # tolerence of size
-contours = {'growth_cnts': [], 'death_cnts': [], 'blotching_cnts': [], 'recovery_cnts': []}
-while True:
-    # Capture webcame frame
-    frame = cv.flip(cap.read()[1], 1)
-    frame = simplest_cb(frame, 1)
-    is_matching, number_of_matches = check_for_matches(frame, old_image, debug=DEBUG)
-    # If the current frame matches the old image
-    if is_matching:
-        # Increment the sample counter by 1
-        SAMPLE_COUNTER += 1
-        ratio = adjust_distance(old_image=old_image, frame=frame)
-        if 1 + SIZE_TOLERENCE > ratio > 1 - SIZE_TOLERENCE:
-            if SAMPLE_COUNTER > SAMPLING_RATE:
-                contours = detect(frame, old_image_ext, debug=DEBUG)  # Detect changes
-                SAMPLE_COUNTER = 0  # Reset the sampling counter
-                # filter contours to get only large ones which eliminates contours caused by noise
-                frame = print_contours(frame, contours["growth_cnts"], COLORS["GREEN"], "growth", area=AREA)
-                frame = print_contours(frame, contours["death_cnts"], COLORS["YELLOW"], "death", area=AREA)
-                frame = print_contours(frame, contours["blotching_cnts"], COLORS["RED"], "blotching", area=AREA)
-                frame = print_contours(frame, contours["recovery_cnts"], COLORS["BLUE"], "recovery", area=AREA)
-        elif ratio > 1:
-            print("move forward !!!")
-        elif ratio < 1:
-            print("move backward !!!")
-    else:
-        SAMPLE_COUNTER = 0  # Reset sample counter
-        contours = {'growth_cnts': [], 'death_cnts': [], 'blotching_cnts': [], 'recovery_cnts': []}
-    # Display the result
-    cv.imshow("Object Tracker", frame)
-    FRAME_COUNT += 1
-    # close the stream when enter key is pressed
-    if cv.waitKey(1) == 13:  # 13 is the Enter Key
-        break
-
-# Release camera and close any open windows
-cap.release()
-cv.destroyAllWindows()
->>>>>>> 3f43c3cb0be62ebe0aea2aa41c810be9b5c8b99f
