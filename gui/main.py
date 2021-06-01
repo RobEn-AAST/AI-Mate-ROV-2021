cc170c5a6489f39558a17dcc10c96d7a3ee0bf32
Nourhan <nourhanelyamany@gmail.com>
2021-06-01 22:37:34 +0200

    gui updated with two buttons

--- gui/main.py
+++ gui/main.py
@@ -3,27 +3,43 @@ from tkinter import *
 from stitching import stitchingFunction
 from flying_the_transect_line import read_video
 from task_2_2 import colonyhealthfunction
+
+y = 0
+
+def changeString():
+   y = 5
+
+
 window = tk.Tk()
 window.geometry('500x500')
 window.title("run rov")
 
    # the button for transect line mission 
 
-
-button1 = tk.Button(window, text = "flying transect line" , fg = "red", width = 10, height = 5, command = read_video)
+button1 = tk.Button(window, text = "flying transect line" , fg = "red", width = 13, height = 5, command = read_video)
 button1.place(x=185, y= 100)
 
    # the button for color detection mission
 
-button2 = tk.Button(window, text = "coral color" , fg = "green", width = 10, height = 5, command= colonyhealthfunction)
+button2 = tk.Button(window, text = "coral color" , fg = "green", width = 13, height = 5, command = colonyhealthfunction)
 button2.place(x=185, y= 200)
 
 
    # the button for stitching mission
 
 
-button3 = tk.Button(window, text = "stitching" , fg = "blue", width = 10, height = 5, command = stitchingFunction)
+button3 = tk.Button(window, text = "stitching" , fg = "blue", width = 13, height = 5, command = stitchingFunction)
 button3.place(x=185, y= 300)
+
+   # button for camera 0
+
+button4 = tk.Button(window, text = "camera 0" , fg = "black", width = 10, height = 2, command = changeString)
+button4.place(x=100, y= 400)
+
+   # button for camera 1
+
+button5 = tk.Button(window, text = "camera 1" , fg = "black", width = 10, height = 2, command = changeString)
+button5.place(x=297, y= 400)
 
 
 
