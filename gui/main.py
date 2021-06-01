9b9cdf913995d4b078da4ed5b0f06fbc2f3737d2
Nourhan <nourhanelyamany@gmail.com>
2021-06-01 16:57:48 +0200

    comments added

--- gui/main.py
+++ gui/main.py
@@ -6,12 +6,25 @@ import cv2 as cv
 window = tk.Tk()
 window.geometry('500x500')
 window.title("run rov")
+
+   # the button for transect line mission 
+
+
 button1 = tk.Button(window, text = "flying transect line" , fg = "red", width = 10, height = 5, command = read_video)
 button1.place(x=185, y= 100)
 
+   # the button for color detection mission
+
 button2 = tk.Button(window, text = "coral color" , fg = "green", width = 10, height = 5)
 button2.place(x=185, y= 200)
+
+
+   # the button for stitching mission
+
+
 button3 = tk.Button(window, text = "stitching" , fg = "blue", width = 10, height = 5)
 button3.place(x=185, y= 300)
+
+
 
 window.mainloop()
