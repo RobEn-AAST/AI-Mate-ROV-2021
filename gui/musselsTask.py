17daf93517680a0f5dd0b731817b9eeb8aa656e4
Nourhan <nourhanelyamany@gmail.com>
2021-06-02 02:35:44 +0200

    added task of mussels

---
+++ gui/musselsTask.py
@@ -0,0 +1,18 @@
+def calcLitres(mussels):
+
+    areaSize = 43*8
+    containerArea = 0.25
+    rate =  0.97
+    totalNumberOfMusselss=9632
+    
+
+
+    amount= (mussels*344)/0.25
+
+    totalAmount = amount*rate
+
+
+    print(totalAmount) 
+
+val=float(input("Enter number of mussels : "))
+calcLitres(val)    
