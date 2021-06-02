8a138a9642aae292953faca65afea0a06069530a
karim kohel <kareemkohel@gmail.com>
2021-06-02 12:34:34 +0200

    cleaned file

--- gui/sshConnect.py
+++ gui/sshConnect.py
@@ -1,12 +1,10 @@
 import paramiko
-import sys
 
-piIp = "192.168.2.2"
-piUser = "pi"
-piPass = "companion"
-
-out = []
 def connection():
+
+    piIp = "192.168.2.2"
+    piUser = "pi"
+    piPass = "companion"
 
     client = paramiko.SSHClient()
     client.load_system_host_keys()
@@ -15,6 +13,5 @@ def connection():
     client.connect(piIp, piUser, piPass)
 
     ssh_stdin , ssh_stdout , ssh_stderr = client.exec_command('bash ROV-Gstreamer/gst0.sh & bash ROV-Gstreamer/gst1.sh ')  # place your gstreamer command here
-
 
 connection()
