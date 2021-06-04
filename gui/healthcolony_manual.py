8afab787b049882dfb27eb22bfd2b484bad2185e
princeofguilty <princeofguilty@hotmail.com>
2021-06-04 06:39:13 +0200

    Update healthcolony_manual.py

--- gui/healthcolony_manual.py
+++ gui/healthcolony_manual.py
@@ -32,9 +32,9 @@ def mouse_drawing(event, x, y, flags, params):
       color_list.append(BLACK)
 
 def coralRead(camInput):
-
+  global read, drawing, s_list, e_list, color_list, num_rect
   cap = cv2.VideoCapture(f'udpsrc port=5{camInput}00 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
-#  cap = cv2.VideoCapture(0)
+  # cap = cv2.VideoCapture(0)
   cv2.namedWindow("Frame")
   cv2.setMouseCallback("Frame", mouse_drawing)
   flag_stop = True
@@ -47,17 +47,33 @@ def coralRead(camInput):
       # cv2.rectangle(frame,s_point,e_point,(0,0,255),0)
         for i in range(num_rect):
           cv2.rectangle(frame,s_list[i],e_list[i],color_list[i],3)
-      
-        if cv2.waitKey(30) & 0xFF == ord('g'):
+        key = cv2.waitKey(30) & 0xFF
+        if key == ord('g'):
           color_list[num_rect-1] = GREEN
-        elif cv2.waitKey(30) & 0xFF == ord('b'):
+        elif key == ord('b'):
           color_list[num_rect-1] = RED
-        elif cv2.waitKey(30) & 0xFF == ord('d'):
+        elif key == ord('d'):
           color_list[num_rect-1] = YELLOW
-        elif cv2.waitKey(30) & 0xFF == ord('r'):
+        elif key == ord('r'):
           color_list[num_rect-1] = BLUE
-    if cv2.waitKey(30) & 0xFF == ord('q'):
+        elif key == ord('c'):
+          drawing = False
+          read = True
+          s_list = []
+          e_list = []
+          color_list = []
+          num_rect = 0
+    key = cv2.waitKey(30) & 0xFF
+    if key == ord('q'):
         flag_stop = False
+    elif key == ord('c'):
+      drawing = False
+      read = True
+      s_list = []
+      e_list = []
+      color_list = []
+      num_rect = 0
+
 
 
     cv2.imshow("Frame", frame)
